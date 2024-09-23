02f4c43d9fbb29323d02b725df85e89f7ce7c2f7
Nikola <nikola88393@gmail.com>
2024-09-23 18:31:17 +0300

    Create view for the index page and render messages

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const indexRouter = require("./routes/indexRoute");
 const messageRouter = require("./routes/messageRouter");
+const path = require("node:path");
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 app.use("/", indexRouter);
 app.use("/new", messageRouter);
 
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,27 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+  {
+    text: "Welcome!",
+    user: "Alex",
+    added: new Date(),
+  },
+  {
+    text: "Good so see you!",
+    user: "Tyrone",
+    added: new Date(),
+  },
+];
 module.exports = {
   get: (req, res) => {
-    res.end("Hello from index");
+    res.render("index", { title: "Home", messages: messages });
   },
 };
