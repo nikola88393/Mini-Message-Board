67472fdf0d3ecb3c85e449eb7e71ab07c00f4129
Nikola <nikola88393@gmail.com>
2024-09-23 20:21:33 +0300

    Implement opening message on a single page

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,4 +3,9 @@ module.exports = {
   get: (req, res) => {
     res.render("index", { title: "Home", messages: messages });
   },
+  getById: (req, res) => {
+    const id = req.params.id;
+    const message = messages.find((message) => message.id == id);
+    res.render("viewMessage", { message: message });
+  },
 };
--- routes/indexRoute.js
+++ routes/indexRoute.js
@@ -3,5 +3,6 @@ const controller = require("../controllers/indexController");
 const route = Router();
 
 route.get("/", controller.get);
+route.get("/:id([0-9]+)", controller.getById);
 
 module.exports = route;
