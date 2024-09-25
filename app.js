6a05199a11ba1a8802e9e1fcd4ca2a6827f49aff
Nikola <nikola88393@gmail.com>
2024-09-25 13:56:37 +0300

    Add simple styling to the app

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 const indexRouter = require("./routes/indexRoute");
 const messageRouter = require("./routes/messageRouter");
 const path = require("node:path");
-const assetsPath = path.join(__dirname, "public");
 
-app.use(express.static(assetsPath));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
